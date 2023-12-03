567362cfcb26bff7b116a6cae4bf9a20359a218b
BrayanMaximiano <brayanmaximiano01@gmail.com>
2023-12-03 19:27:15 -0300

    Criação dos getters e setters

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("O filme tem " + meuFilme.getTotalDeAvaliacoes() + " avaliações");
+        System.out.println("A nota média do filme é: " + meuFilme.pegaMedia());
+
+    }
+}
