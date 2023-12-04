8a08f70360bb308493ea5a7aa462854794d28846
BrayanMaximiano <brayanmaximiano01@gmail.com>
2023-12-04 15:02:03 -0300

    Reaproveitando características e comportamentos

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,18 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.rmi.ServerError;
 
 public class Principal {
     public static void main(String[] args) {
+        // Cria filme
         Filme meuFilme = new Filme();
-
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+
+        System.out.println("Duraçao do filme é de " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,6 +21,30 @@ public class Principal {
 
         System.out.println("O filme tem " + meuFilme.getTotalDeAvaliacoes() + " avaliações");
         System.out.println("A nota média do filme é: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        System.out.println("Duração para maratonar : " + lost.getDuracaoEmMinutos());
+
+        // Cria filme
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println("Duraçao total da playlist: " + calculadora.getTempoTotal());
+
 
     }
 }
