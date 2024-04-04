449c735d42eb7df5a10c3af31ca9db9bd237ecee
Brayan Maximiano <brayanmaximiano01@gmail.com>
2024-04-04 11:44:06 -0300

    Transformando dados

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,12 +1,22 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+
+    @SerializedName("Year")
     private int anoDeLancamento;
+
     private boolean incluidoNoPlano;
+
     private double somaDasAvaliacoes;
+
     private int totalDeAvaliacoes;
+
     private int duracaoEmMinutos;
+
 
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
